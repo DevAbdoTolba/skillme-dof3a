ba1e7a6cc296a271187389d9fc68f616b5c8dc0f
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-01-19 09:47:58 +0200

    working backend

---
+++ pages/api/v1/getAll.ts
@@ -0,0 +1,44 @@
+import { google, sheets_v4 } from "googleapis";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { range } = req.query;
+
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      type: process?.env?.TYPE,
+      project_id: process?.env?.PROJECT_ID,
+      private_key_id: process?.env?.PRIVATE_KEY_ID,
+      private_key: process?.env?.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      client_email: process?.env?.CLIENT_EMAIL,
+      client_id: process?.env?.CLIENT_ID,
+      auth_uri: process?.env?.AUTH_URI,
+      token_uri: process?.env?.TOKEN_URI,
+      auth_provider_x509_cert_url: process?.env?.AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process?.env?.CLIENT_X509_CERT_URL,
+    },
+    scopes: [
+      "https://www.googleapis.com/auth/drive",
+      "https://www.googleapis.com/auth/drive.file",
+      "https://www.googleapis.com/auth/drive.readonly",
+      "https://www.googleapis.com/auth/spreadsheets",
+      "https://www.googleapis.com/auth/spreadsheets.readonly",
+    ],
+  });
+
+  const client = await auth.getClient();
+  const sheets = google.sheets({
+    version: "v4",
+    auth: client,
+  }) as sheets_v4.Sheets;
+
+  const response = await sheets.spreadsheets.values.batchGet({
+    spreadsheetId: "1FiIbZvrED_QvbLdlLECgb1tyqP2MUl5BGyEJRxUcWyA",
+    ranges: [range as string],
+  });
+
+  res.status(200).json(response.data);
+}
