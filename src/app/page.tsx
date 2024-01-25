35a8458c30be8923ea3862e18697980fa58032bb
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-01-25 17:16:35 +0200

    Refactor layout and page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
 "use client";
-import Data from "./Data/page";
-import { CssBaseline } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 export default function Main() {
-  const theme = createTheme({
-    palette: {
-      mode: "dark",
-    },
-  });
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Data />
-      </ThemeProvider>
-    </>
-  );
+  return <>Hi</>;
 }
